94aa0351ccd9d2122c2efd0b41bcbf8fb288b838
Souheilaso <souheila.sidennas@gmail.com>
2023-08-11 13:46:24 +0100

    variables

--- 01-Fundamentals/script.js
+++ 01-Fundamentals/script.js
@@ -1,4 +1,8 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Java is fun');
-
 console.log(40 + 8 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'John';
+console.log(firstName);
