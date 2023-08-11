bf6b3155cc300c8d11bba4d73e808407605de8a8
Souheilaso <souheila.sidennas@gmail.com>
2023-08-11 14:22:43 +0100

    data types

--- 01-Fundamentals/script.js
+++ 01-Fundamentals/script.js
@@ -7,10 +7,25 @@ console.log(23);
 let firstName = 'John';
 console.log(firstName);
 
-let country = 'Portugal';
-let continent = 'Canada';
-let population = '1000000'
+// Declare variables and assign values
+let country = "United States";
+let continent = "North America";
+let population = 331;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// Log values to the console
+console.log("Country:", country);
+console.log("Continent:", continent);
+console.log("Population:", population);
+
+
+// data types 
+// Declare variables and set values
+let isIsland = false;
+let language;
+
+// Log types to the console
+console.log("Type of 'isIsland':", typeof isIsland);
+console.log("Type of 'population':", typeof population);
+console.log("Type of 'country':", typeof country);
+console.log("Type of 'language':", typeof language);
+
