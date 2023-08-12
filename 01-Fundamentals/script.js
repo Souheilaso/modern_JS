d646109f12bf5a279bd40e2ac567b880c2533104
Souheilaso <souheila.sidennas@gmail.com>
2023-08-12 16:11:43 +0100

    basic operators test

--- 01-Fundamentals/script.js
+++ 01-Fundamentals/script.js
@@ -1,38 +1,44 @@
-let js = 'amazing';
-console.log(40 + 8 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 - 10);
 
-console.log('Jonas');
-console.log(23);
+// console.log('Jonas');
+// console.log(23);
 
-let firstName = 'John';
-console.log(firstName);
+// let firstName = 'John';
+// console.log(firstName);
 
-// Declare variables and assign values
-let country = "United States";
-let continent = "North America";
-let population = 331;
+// // Declare variables and assign values
+// let country = "United States";
+// let continent = "North America";
+// let population = 331;
 
-// Log values to the console
-console.log("Country:", country);
-console.log("Continent:", continent);
-console.log("Population:", population);
+// // Log values to the console
+// console.log("Country:", country);
+// console.log("Continent:", continent);
+// console.log("Population:", population);
 
 
-// data types 
-// Declare variables and set values
-let isIsland = false;
-let language;
+// // data types 
+// // Declare variables and set values
+// let isIsland = false;
+// let language;
 
-// Log types to the console
-console.log("Type of 'isIsland':", typeof isIsland);
-console.log("Type of 'population':", typeof population);
-console.log("Type of 'country':", typeof country);
-console.log("Type of 'language':", typeof language);
+// // Log types to the console
+// console.log("Type of 'isIsland':", typeof isIsland);
+// console.log("Type of 'population':", typeof population);
+// console.log("Type of 'country':", typeof country);
+// console.log("Type of 'language':", typeof language);
 
-// let, const
-let age = 19;
-age = 29;
-const yearBirth = 1900;
+// // let, const
+// let age = 19;
+// age = 29;
+// const yearBirth = 1900;
 
-// lecture assignment
-language = "Portuguese";
+// // lecture assignment
+// language = "Portuguese";
+
+// Basic Operators
+const year = 2037
+const ageJonas = year - 1991;
+const ageSarah = year - 2018;
+console.log(ageJonas, ageSarah);
