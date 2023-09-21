1d9220b386ea961a7905ae4bd1b14e38011a0283
Souheilaso <souheila.sidennas@gmail.com>
2023-09-21 09:32:08 +0100

    Truthy and falsy

--- 01-Fundamentals/script.js
+++ 01-Fundamentals/script.js
@@ -116,16 +116,30 @@
 // console.log(century);
 
 // Type conversion
-const inputYear = '1991';
-console.log(Number(inputYear));
-console.log(inputYear + 18);
-console.log(Number(inputYear) + 18);
-console.log(String(23));
+// const inputYear = '1991';
+// console.log(Number(inputYear));
+// console.log(inputYear + 18);
+// console.log(Number(inputYear) + 18);
+// console.log(String(23));
 
-// Type coercion
-console.log("I am " + 23 + " years old")
-console.log('23' - '10 ' - 3);
+// // Type coercion
+// console.log("I am " + 23 + " years old")
+// console.log('23' - '10 ' - 3);
 
-let n = 1 + '1'; // this turns to a string
-n = n - 1; // the minus operator converts strings to numbers
-console.log(n);
+// let n = 1 + '1'; // this turns to a string
+// n = n - 1; // the minus operator converts strings to numbers
+// console.log(n);
+
+// Truthy and falsy values
+// Falsy are 0, '', undefined, Nan, or null
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all");
+} else {
+    console.log("You should get a job!");
+}
